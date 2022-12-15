9554e5b08a3fa983c6136d90da59e9f53f2b1419
Daniel Gutierrez <gutierrez.navarro.daniel@gmail.com>
2022-12-15 16:55:04 +0100

    Corrections main function, number classifier

--- Number_classifier/fixed_support.py
+++ Number_classifier/fixed_support.py
@@ -13,9 +13,11 @@ def fixed_support(c, p_k):
     # Obj
     obj = cp.Minimize(epsilon)
 
+    cons = []
+
     # Cons
     for k in range(K):
-        cons = [T[k] >= 0]
+        cons.append(T[k] >= 0)
         #cons.append(sum(sum(cp.multiply(T[k], c))) <= epsilon)
         cons.append(cp.trace(c.T@T[k]) <= epsilon)
         cons.append(T[k]@np.ones(N) == p)
@@ -25,4 +27,24 @@ def fixed_support(c, p_k):
     problem = cp.Problem(obj, cons)
     problem.solve()
     
-    return p.value
+    return (epsilon.value, p.value)
+
+def wasserstein_dist(c, p, p_k):
+    N = len(p_k)
+
+    # Dec Var
+    T = cp.Variable((N, N))
+
+    # Obj
+    obj = cp.Minimize(cp.trace(c.T@T))
+
+    # Cons
+    cons = [T >= 0]
+    cons.append(T@np.ones(N) == p)
+    cons.append(T.T@np.ones(N) == p_k)
+
+    # Solve program
+    problem = cp.Problem(obj, cons)
+    problem.solve()
+
+    return obj.value
